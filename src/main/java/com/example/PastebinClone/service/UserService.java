80711af1e6b7bc9b85f830b2577fa38ce53ebff9
Cristian Marinescu <97915515+CristianM92@users.noreply.github.com>
2023-03-12 19:47:49 +0200

    Update UserService.java

--- src/main/java/com/example/PastebinClone/service/UserService.java
+++ src/main/java/com/example/PastebinClone/service/UserService.java
@@ -5,6 +5,5 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface UserService extends UserDetailsService {
-
      User save(UserRegistrationDTO registrationDTO);
 }
